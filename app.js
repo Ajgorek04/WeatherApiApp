ec0411160868cc74a543d5225e4e215cdaf15b30
Ajgorek <igorblendzinski12@gmail.com>
2023-09-06 14:52:30 +0200

    Finished styling css

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`;
+const button = document.querySelector("#button");
+const input = document.querySelector("#input");
+
+button.addEventListener("click", () => {
+    console.log(input.value);
+    wiem = input.value;
+    input.value = "";
+
+    checkWeather(wiem);
+});
+
 const API_KEY = "53522ab6c9e4a4f6ed4f713d97ca6bed";
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?&units=metric`;
+
+async function checkWeather(city) {
+    const response = await fetch(API_URL + `&q=${city}` + `&appid=${API_KEY}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    console.log(data.main.temp);
+    console.log(data.weather[0].main);
+    console.log(data.wind.speed);
+}
